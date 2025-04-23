57d441fcd68d5676df1f737e5504bc76c8233591
ayham169 <ayhamaboras@icloud.com>
2025-04-23 18:44:08 +0200

    Initial commit

---
+++ resources/views/content.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.app')
+@section('content')
+
+       <h1>ayahm</h1>
+       <tr>
+              <td>Name</td>
+              <td>Price</td>
+       </tr>
+       @foreach ($prode as $g)
+           
+       @endforeach
+@endsection
